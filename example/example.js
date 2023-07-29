5f07a6cbdc955c1439ca95e534e9a9ddeaa0da30
only1drhex <lordrhex@gmail.com>
2023-07-29 10:15:17 +0100

    readme

--- example/example.js
+++ example/example.js
@@ -5,7 +5,7 @@ const ENCRYPTION_KEY = 'your_secret_key_here';
 // Replace with your secret key
 
 // Example 1: Encrypt and Decrypt data
-const originalData = 'Secret word here!';
+const originalData = 'Encryptly - encrypt and Decrypt.';
 const encryptedData = encryptly.encrypt(originalData, ENCRYPTION_KEY);
 const decryptedData = encryptly.decrypt(encryptedData, ENCRYPTION_KEY);
 
